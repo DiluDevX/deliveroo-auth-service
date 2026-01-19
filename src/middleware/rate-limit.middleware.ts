c5811a4247412ca4dd3da261d0d0e37f36178944
Dilum Paranavithana <dilumchandupa@gmail.com>
2026-01-19 22:48:09 +0530

    feat(rate-limits): reduce maximum requests for health endpoint rate limiter from 100 to 10

--- src/middleware/rate-limit.middleware.ts
+++ src/middleware/rate-limit.middleware.ts
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const healthRateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 100,
+  max: 10,
   message: {
     status: 429,
     message: 'Too many requests from this IP, please try again later.',
